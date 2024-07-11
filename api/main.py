9b5ef432411f17fff2755cb72a343d578c073588
Jesús S Guerrero <jesus_sguerrero@hotmail.com>
2024-07-11 17:40:09 -0600

    filter by value, and pgadmin

--- api/main.py
+++ api/main.py
@@ -1,28 +1,56 @@
 from flask import Flask, jsonify, request
-from model.db_layer import DatabaseLayer
+from model.db_layer import DatabaseLayer, EDO
 
 app = Flask(__name__)
 db = DatabaseLayer()
 
-@app.route('/edos', methods=['GET'])
+@app.route('/get_edos', methods=['POST'])
 def get_edos():
     try:
-        edos = db.get_edos()
+        data = request.json
+        if not _is_valid(data):
+            return jsonify({'message': f"Unexpected data on request {type(data['names'])}"}), 400
+        edos, message = db.get_edos(
+            ids=data.get('ids', []),
+            names=data.get('names', []),
+            mobile_numbers=data.get('mobile_numbers', []),
+            emails=data.get('emails', []),
+            addresses=data.get('addresses', [])
+        )
         r = {'data': edos}
         http_code = 200
+        if message:
+            r['message'] = message
         if not edos:
             r['message'] = 'data not found'
             http_code = 400
         return jsonify(r), http_code
     except Exception as error:
         app.logger.debug('db error: ', error)
-        return jsonify({'message': 'Unexpected error'})
+        return jsonify({'message': 'Unexpected error'}), 500
 
-@app.route('/edos', methods=['POST'])
+@app.route('/post_edo', methods=['POST'])
 def post_edos():
-    if not request.json or 'title' not in request.json or 'author' not in request.json:
+    if not request.json or 'name' not in request.json:
         return jsonify({'message': 'Missing data on request'}), 400
     
+    edo = EDO(
+        request.json['name'], 
+        request.json.get('mobileNumber', ''), 
+        request.json.get('email', ''),
+        request.json.get('address', '')
+    )
+    err = db.post_edo(edo)
+    if err is not None:
+        return jsonify({'message': f"Unexpected error {err}"}), 500
+    return jsonify({'message': 'success'}), 200
+
+def _is_valid(data):
+    params = ['ids', 'names', 'mobile_numbers', 'emails', 'addresses']
+    for param in params:
+        if param in data and type(data[param]) is not list:
+            return False
+    return True
 
 if __name__ == "__main__":
     app.run(debug=True)
--- api/model/db_layer.py
+++ api/model/db_layer.py
@@ -1,27 +1,87 @@
 import psycopg2
 from psycopg2 import Error
 
+class EDO:
+    def __init__(self, name, mobile_number="", email="", address="") -> None:
+        self.name = name
+        self.mobile_number = mobile_number
+        self.email = email
+        self.address = address
+
+class Query:
+    def __init__(self, ids=[], names=[], mobile_numbers=[], emails=[], addresses=[]):
+        self.filter_values = {}
+        if ids:
+            self.filter_values['id'] = ids
+        if names:
+            self.filter_values['name'] = names
+        if mobile_numbers:
+            self.filter_values['mobileNumber'] = mobile_numbers
+        if emails:
+            self.filter_values['email'] = emails
+        if addresses:
+            self.filter_values['physicalAddress'] = addresses
+
+        conditions = []
+        params = []
+
+        for column, values in self.filter_values.items():
+            placeholders = ','.join(['%s'] * len(values))
+            conditions.append(f"{column} IN ({placeholders})")
+            params.extend(values)
+
+        where_clause = " AND ".join(conditions)
+        self.select = "SELECT * FROM edos"
+        self.message = "empty query, returning all records"
+        if where_clause:
+            self.select = f"SELECT * FROM edos WHERE {where_clause}"
+            self.message = ""
+        self.params = tuple(params)
+
 class DatabaseLayer:
-    def get_edos(self):
+    def get_edos(self, ids=[], names=[], mobile_numbers=[], emails=[], addresses=[]):
+        query = Query(ids, names, mobile_numbers, emails, addresses)
         connection = self._db_connection()
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM edos")
-        cursor.close()
-        connection.close()
+        cursor.execute(query.select, query.params)
+        result = []
         if cursor.pgresult_ptr is not None:
             rows = cursor.fetchall()
-        else:
-            return []
-        return [
-            {'id': row[0], 'title': row[1], 'author': row[2]}
-            for row in rows
-        ]
+            result = [
+                {
+                    'id': row[0], 
+                    'name': row[1], 
+                    'mobileNumber': row[2],
+                    'email': row[3],
+                    'physicalAddress': row[4],
+                    'created_at': row[5]
+                }
+                for row in rows
+            ]
+        cursor.close()
+        connection.close()
+        return result, query.message
+    
+    def post_edo(self, edo: EDO):
+        insert_query = """
+        INSERT INTO edos (name, mobileNumber, email, physicalAddress) 
+        VALUES (%s, %s, %s, %s);
+        """
+        try:
+            connection = self._db_connection()
+            cursor = connection.cursor()
+            cursor.execute(insert_query, (edo.name, edo.mobile_number, edo.email, edo.address))
+            connection.commit()
+            cursor.close()
+            connection.close()
+        except Exception as error:
+            return str(error)
     
     def _db_connection(self):
         try:
             return psycopg2.connect(
                 dbname='edo_db',
-                user='user',
+                user='admin',
                 password='password',
                 host='db',
                 port='5432'
